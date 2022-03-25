e22ac17dcef9e2efbc991017af4d41056c22b1d6
rrangel07 <99489197+rrangel07@users.noreply.github.com>
2022-03-25 12:27:49 -0400

    First commit: initial html, initial javaScript

---
+++ assets/script.js
@@ -0,0 +1,66 @@
+var timer= document.querySelector('.timer')
+var timeLeft = 60;
+var possibleQuestions= [{
+    question: "Carrie Bradshaw's obsession was?",
+    answer: ['Shoes','Purses','Earings', 'Dresses'],
+    correctAnswer: 0,
+    },
+    {
+    question: "Miranda was a",
+    answer: ['Pediatrician','Engineer','Corporate Lawyer', 'Designer'],
+    correctAnswer: 2,
+    },
+    {
+    question: "Who was Charlotte's first husband?",
+    answer: ['Mr. Big','Aidan','Jack Berger', 'Trey MacDougal'],
+    correctAnswer: 3,
+    },
+    {
+    question: "Samantha Jones dated a woman named?",
+    answer: ['Petra','Maria','Eugenia', 'Rose'],
+    correctAnswer: 1,
+    },
+    {
+    question: "Magda replaced Miranda's vibrator with what",
+    answer: ['A statue of the Virgin Mary','The Bible','Holy Water', 'A Crucifix'],
+    correctAnswer: 0,
+    },
+    {
+    question: "Which was Carrie Bradshaw's favorite shoe brand?",
+    answer: ['Jimmy Choo','Chanel','Nike', 'Manolo Blahnik'],
+    correctAnswer: 3,
+    },
+    {
+    question: "Which store did Carrie fall in in Paris?",
+    answer: ['Chanel','Dior','CH', 'Oscar de la Renta'],
+    correctAnswer: 1,
+    },
+    {
+    question: "Who dumped Carrie via a Post-it?",
+    answer: ['Aiden','Big','Steve', 'Jack Berger'],
+    correctAnswer: 3,
+    },
+    {
+    question: "What year did 'Sex and the city' premiere?",
+    answer: ['1992','1996','1998', '1997'],
+    correctAnswer: 2,
+    },
+    {
+    question: "When did I hate Carrie the most?",
+    answer: ['When she cheated on Aiden with Big','When she dumped Aiden for the 2nd time','All of the above', 'When she didn\'t call 911 during big\'s heart attack'],
+    correctAnswer: 2,
+    }
+];
+// for(let i=0; i<possibleQuestions.length; i++){
+
+//     for (let j=0; j<possibleQuestions[i].answer.length; j++){
+//         console.log(possibleQuestions[i].answer[j]);
+//     }
+// }
+
+function startTimer (){
+    var time= setInterval(function(){
+        if( )
+
+    })
+}
